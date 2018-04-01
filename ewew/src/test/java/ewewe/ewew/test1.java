b65b6add7003ce911bb23143f923dc4d0381a3ac
nikhil <Dell@DESKTOP-CPJVQ9L>
2018-04-01 14:43:18 +0530

    this is to load project in local  rep

---
+++ ewew/src/test/java/ewewe/ewew/test1.java
@@ -0,0 +1,19 @@
+package ewewe.ewew;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+	public static void main(String args[]){
+//test
+	System.setProperty(
+			"webdriver.chrome.driver",
+			"\\"
+					+ "F:\\selenium\\chromedriver.exe");
+
+	WebDriver driver = new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://www.gmail.com");
+
+}
+}
