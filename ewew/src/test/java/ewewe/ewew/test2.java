180dc38248a58d7a8f4b55c21e56898470503bcc
vikky <vikky@vipul>
2018-04-04 00:25:33 +0530

    ADDED @TEST ANNOATAION

--- ewew/src/test/java/ewewe/ewew/test2.java
+++ ewew/src/test/java/ewewe/ewew/test2.java
@@ -1,19 +1,24 @@
 package ewewe.ewew;
 
+import javax.swing.SortingFocusTraversalPolicy;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class test2 {
-	public static void main(String args[]){
+	@Test
+	public void test2(){
 
 		System.setProperty(
 				"webdriver.chrome.driver",
 				"\\"
-						+ "F:\\selenium\\chromedriver.exe");
+						+ "E:\\selenium\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.fb.com");
+		System.out.println("fb");
 
 	}
 
--- ewew/src/test/java/ewewe/ewew/test3.java
+++ ewew/src/test/java/ewewe/ewew/test3.java
@@ -2,19 +2,21 @@ package ewewe.ewew;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class test3 {
-	public static void main(String args[]){
+	@Test
+	public void test3(){
 
 		System.setProperty(
 				"webdriver.chrome.driver",
 				"\\"
-						+ "F:\\selenium\\chromedriver.exe");
+						+ "E:\\selenium\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.amazon.com");
-
+		System.out.println("amazon");
 	}
 
 
